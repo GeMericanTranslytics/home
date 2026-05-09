41a95b30e3fabc98b7e25897b770633da17b0d33
GeMericanTranslytics <mhfirmont@outlook.com>
2026-05-09 13:54:01 -0600

    Update projects section with background color
    Added a background color to the projects section.

--- components/projects.tsx
+++ components/projects.tsx
@@ -188,7 +188,7 @@ export function Projects() {
   )
 
   return (
-    <section id="projects" className="py-24">
+    <section id="projects" className="py-24 bg-black/50">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-6xl mx-auto">
           
